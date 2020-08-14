a79a4d48076f3e9f1f8f7f125dbb80ba1b56bed1
Akshay Taphare <taphareakshay@gmail.com>
2020-08-14 13:52:04 +0530

    React-Redux-Form

---
+++ src/components/ContactComponent.js
@@ -0,0 +1,227 @@
+import React,{ Component } from 'react';
+import { Breadcrumb ,BreadcrumbItem ,Button,Form,FormGroup,Label,Input,Col,Row,FormFeedback} from 'reactstrap';
+import { Control,LocalForm,Errors } from 'react-redux-form';
+import {Link } from 'react-router-dom';
+
+class Contact extends Component{
+
+    constructor(props){
+        super(props);
+        {/* set all properties in state */}
+
+       {/*this.state={
+                firstname : '',
+                lastname : '',
+                telnum : '',
+                email : '',
+                agree : false,
+                contactType : 'Tel.',
+                message : '',
+                touched:{
+                    firstname:false,
+                    lastname:false,
+                    telnum:false,
+                    email:false
+                }
+
+        };*/}
+        this.handleSubmit=this.handleSubmit.bind(this);
+       /* this.handleInputChange=this.handleInputChange.bind(this);
+       this.handleBlur=this.handleBlur.bind(this);*/
+    }
+    /*handleInputChange(event){
+        const target=event.target;
+        const value=target.type === 'checkbox' ? target.checked : target.value;
+        const name=target.name;
+
+        this.setState({
+            [name] : value
+        });
+
+    }*/
+    handleSubmit(values){
+        console.log('Current State Is : '+JSON.stringify(values));
+        alert('Current State Is : '+JSON.stringify(values));
+       
+    }
+
+    /*handleBlur =(field) =>(evt) =>{
+            this.setState({
+                touched:{ ...this.state.touched,[field]:true}
+            });
+    }*/
+
+    /*validate(firstname,lastname,telnum,email){
+        const error={
+            firstname : '',
+            lastname : '',
+            telnum : '',
+            email : ''
+        };
+        if(this.state.touched.firstname && firstname.length < 3){
+            error.firstname ="First Name Should Be >=3 Character ";
+        }
+        else if(this.state.touched.firstname && firstname.length > 10){
+            error.firstname="First Name Should Be <=10 Character" ;
+        }
+         
+        if(this.state.touched.lastname && lastname.length < 3){
+            error.lastname ="First Name Should Be >=3 Character ";
+        }
+        else if(this.state.touched.lastname && lastname.length > 10){
+            error.lastname="First Name Should Be <=10 Character" ;
+        }
+
+        const reg=/^\d+$/;
+        if(this.state.touched.telnum && !reg.test(telnum)){
+                error.telnum="Tel. Number should contains only Number";
+        }
+
+        if(this.state.touched.email && email.split('').filter(x=> x==='@').length!= 1){
+            error.email="Email Should contains a @ "
+        }
+        return error;
+
+    }*/
+    render(){
+        //const error=this.validate(this.state.firstname,this.state.lastname,this.state.telnum,this.state.email);
+
+        return(
+            <div className="container">
+                {/* Breadcrumb use krun karaych */}
+                <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>Contact Us</h3>
+                            <hr />
+                       </div>
+                </div>
+
+                
+                <div className="row row-content">
+                    <div className="col-12">
+                    <h3>Location Information</h3>
+                    </div>
+                    <div className="col-12 col-sm-4 offset-sm-1">
+                            <h5>Our Address</h5>
+                            <address>
+                            121, Clear Water Bay Road<br />
+                            Clear Water Bay, Kowloon<br />
+                            HONG KONG<br />
+                            <i className="fa fa-phone"></i>: +852 1234 5678<br />
+                            <i className="fa fa-fax"></i>: +852 8765 4321<br />
+                            <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                            </address>
+                    </div>
+                    <div className="col-12 col-sm-6 offset-sm-1">
+                        <h5>Map of our Location</h5>
+                    </div>
+                    <div className="col-12 col-sm-11 offset-sm-1">
+                        <div className="btn-group" role="group">
+                            <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                            <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
+                            <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                        </div>
+                    </div>
+                </div>
+                <div  className="row row-content">
+                    <div className="col-12">
+                        <h3>Send Us A Feddback</h3>
+                    </div>
+                    <div className="col-12 col-md-9">
+                        <LocalForm onSubmit={(values)=>this.handleSubmit(values)}>
+                            {/* row-->yek row provide krte */}
+                            <Row className="form-group">
+                                <Label htmlFor="firstname"  for="firstname" md={2}>First Name</Label>
+                                {/* <div clasName="col-md-10"></div>}  -> sarkh col kam krt */}
+                                <Col md={10}>
+                                    {/* by using value we tight this element to controlled component state . Mnaje khi changes kele tri hyachta te direct react component mdhe disnar */}
+                                    <Control.text model=".firstname"  id="firstname" name="firstname"
+                                     placeholder="First Name"  
+                                     className="form-control"
+                                      />
+                                  
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="lastname" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Control.text model=".lastname"  id="lastname" name="lastname"
+                                        placeholder="Last Name"
+                                        className="form-control"
+                                         />
+                                       
+                                </Col>                        
+                            </Row>
+                            <Row className="form-group">
+                            <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
+                                <Col md={10}>
+                                    <Control.text model=".telnum" id="telnum" name="telnum"
+                                        placeholder="Tel. number"
+                                        className="form-control"
+                                        />
+                                      
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="email" md={2}>Email</Label>
+                                <Col md={10}>
+                                    <Control.text model=".email"  id="email" name="email"
+                                        placeholder="Email"
+                                        className="form-control"
+                                    />                                   
+                                </Col>
+                            </Row>
+
+                            <Row className="form-group">
+                                <Col md={{ size : 6 , offset:2}}> {/* right la shift karnyasathi */}
+                                    <div className="form-check">
+                                       <Label check>
+                                           <Control.checkbox model=".agree" name="agree" 
+                                               className="form-check-input"
+                                             />{' '}
+                                           <strong>May we conatct you ?</strong>
+                                        </Label>
+                                    </div>   
+                                </Col>
+                                <Col md={{size : 3, offset : 1}}>
+                                    <Control.select model=".contactType"  name="select" className="form-control" >
+                                        <option>Tel.</option>
+                                        <option>Email</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+
+                            <Row className="form-group">
+                                <Label htmlFor="message" md={2}>Your Feedback</Label>
+                                <Col md={10}>
+                                    <Control.textarea model=".message" id="message" name="message"
+                                        rows="12"
+                                        className="form-control" >
+                                     </Control.textarea>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{size: 10, offset: 2}}>
+                                    <Button type="submit" color="primary">
+                                        Send Feedback
+                                    </Button>
+                                </Col>
+                            </Row>
+
+                           
+                        </LocalForm>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+export default Contact;
